ef086d1ca2711a5110febb0739b4d0b18cda3861
DIN <dinstoree41@gmail.com>
2026-09-08 03:41:19 +0700

    Update ehi.keys.js

--- config/ehi.keys.js
+++ config/ehi.keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-
   L1_KEY: Buffer.from(
     "7e1210f7aab956f7a668bda6e57feddb7f84ad840aef8d27b1b969959be3ab6c",
     "hex"
@@ -10,46 +9,26 @@ module.exports = {
     "hex"
   ),
 
-  EOO_MASTER_KEY:
-    Buffer.from(
-      "null=V5kU5+FFrY\0",
-      "utf8"
-    ),
+  EOO_MASTER_KEY: Buffer.from(
+    "null=V5kU5+FFrY",
+    "utf8"
+  ),
 
   BYPASS_IVS: [
-    Buffer.from(
-      "221d572349555f1d112133236b1f4a3f",
-      "hex"
-    ),
-    Buffer.from(
-      "5543494c53443e3f4a6a4539384e776a",
-      "hex"
-    ),
-    Buffer.from(
-      "374c2541575e4d531a3c327b75431e5f",
-      "hex"
-    )
+    Buffer.from("221d572349555f1d112133236b1f4a3f", "hex"),
+    Buffer.from("5543494c53443e3f4a6a4539384e776a", "hex"),
+    Buffer.from("374c2541575e4d531a3c327b75431e5f", "hex")
   ],
 
   STANDARD_IVS: [
-    Buffer.from(
-      "2c5d1147bbad422b3b334d4d235f1a53",
-      "hex"
-    ),
-    Buffer.from(
-      "522b01433a5e8b2fc7549e1ad368e541",
-      "hex"
-    ),
-    Buffer.from(
-      "337a1035aaedf3458ca167e92d74b839",
-      "hex"
-    )
+    Buffer.from("2c5d1147bbad422b3b334d4d235f1a53", "hex"),
+    Buffer.from("522b01433a5e8b2fc7549e1ad368e541", "hex"),
+    Buffer.from("337a1035aaedf3458ca167e92d74b839", "hex")
   ],
 
-  STD_ALPHABET:
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
-
   CUSTOM_ALPHABET:
-    "RkLC2QaVMPYgGJW/A4f7qzDb9e+t6Hr0Zp8OlNyjuxKcTw1o5EIimhBn3UvdSFXs"
+    "RkLC2QaVMPYgGJW/A4f7qzDb9e+t6Hr0Zp8OlNyjuxKcTw1o5EIimhBn3UvdSFXs",
 
+  STD_ALPHABET:
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 };
