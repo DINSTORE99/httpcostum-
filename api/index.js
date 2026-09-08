33407c98bef6d0b61185f0e743ff295e500f8582
DIN <dinstoree41@gmail.com>
2026-09-08 11:59:10 +0700

    Remove extra newline in index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-
 const hcDecrypt = require("../decryptors/hc");
 const ehiDecrypt = require("../decryptors/ehi");
 
